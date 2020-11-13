22526f73d941e40e0c8dae2ed030e7290ebca13b
TheBoje <louis.leenar@gmail.com>
2020-11-13 23:00:42 +0100

    Implémentation de col_addVoitureSansTri

--- Collection.c
+++ Collection.c
@@ -3,33 +3,47 @@
  * Auteurs : Louis Leenart & Vincent Commin
  ********************************************************************/
 
+#include <stdlib.h>
+
 #include <Collection.h>
 
 /*----------*
  * définition de la structure
  *----------*/
 
-struct element
+typedef struct Element
 {
     Voiture voiture;
-    struct element * precedent;
-    struct element * suivant;
-};
+    Element * precedent;
+    Element * suivant;
+} Element;
 
 struct CollectionP
 {
-    struct element * premier;
-    struct element * dernier;
+    Element * premier;
+    Element * dernier;
 };
 
 /*----------*
  * initialisation de la structure
  *----------*/
 
-Collection col_creer();
-Collection col_creerCopie(const_Collection source);
+// @brief Créé une collection initialisée en tant que collection vide
+Collection col_creer()
+{
+    Collection result;
+    result->premier = NULL;
+    result->dernier = NULL;
+    return result;
+}
 
-void col_detruire(Collection *pself);
+// @brief Créé une collection a partir d'une autre collection (clone)
+Collection col_creerCopie(const_Collection source)
+{
+    Collection result;
+}
+
+void col_detruire(Collection *pself); 
 
 void col_vider(Collection self);
 
@@ -41,7 +55,35 @@ int col_getNbVoitures(const_Collection self);
 // on récupère une copie de la voiture
 Voiture col_getVoiture(const_Collection self, int pos);
 
-void col_addVoitureSansTri(Collection self, const_Voiture voiture);
+void col_addVoitureSansTri(Collection self, const_Voiture voiture)
+{
+    Element * element = malloc(sizeof(Element));
+    
+    // Dans le cas ou la mémoire n'est pas allouée correctement, le programme échoue
+    if (element == NULL)
+    {
+        fprintf(stderr, "Error:Collection - addVoitureSansTri - mem alloc failed");
+        exit(EXIT_FAILURE);
+    }
+
+    element->voiture = voiture;
+
+    element->suivant = NULL;
+    
+    if (self->dernier != NULL)
+    {
+        (self->dernier)->suivant = element;
+        self->dernier = element;
+
+        element->precedent = self->dernier;
+    }
+    else
+    {
+        self->premier = element;
+        self->dernier = element;
+        element->precedent = NULL;
+    }
+}
 void col_addVoitureAvecTri(Collection self, const_Voiture voiture);
 
 void col_supprVoitureSansTri(Collection self, int pos);
